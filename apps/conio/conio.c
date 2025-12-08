274f347d90b40c63cfcc3643411c57756577079f
Toby Jaffey <toby@ringtailsoftware.co.uk>
2025-12-08 10:35:42 +0000

    Break console io example out into own app

---
+++ apps/conio/conio.c
@@ -0,0 +1,15 @@
+#define USE_MAIN
+#include "uvm32_target.h"
+
+void main(void) {
+    uint32_t c;
+    println("Press a key!");
+    while(c = getc()) {
+        if (c != 0xFFFFFFFF) {
+            print("Got: ");
+            printx(c);
+            println("");
+        }
+    }
+}
+
--- apps/helloworld/helloworld.c
+++ apps/helloworld/helloworld.c
@@ -5,15 +5,5 @@ void main(void) {
     for (int i=0;i<10;i++) {
         printd(i);
     }
-#if 0
-    uint32_t c;
-    while(c = getc()) {
-        if (c != 0xFFFFFFFF) {
-            print("Got: ");
-            printx(c);
-            println("");
-        }
-    }
-#endif
 }
 
